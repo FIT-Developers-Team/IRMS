e0079d3522b04d038eee32f0c30f03db0eece391
initial project drafting <andry.apriyadi@astronauts.id>
2026-08-11 01:04:23 +0700

    chore(config): revert superset baseUrl to proxy /superset-api due to CORS

--- src/config/googleSheets.js
+++ src/config/googleSheets.js
@@ -19,7 +19,7 @@ export const GOOGLE_SHEETS_CONFIG = {
     sohwh: 'SOHWH'
   },
   superset: {
-    baseUrl: 'https://dash.astronauts.id',
+    baseUrl: '/superset-api',
     datasourceId: 348,
     cookieSpreadsheetId: '1Clj9YvTa6zaFnuEZI0eSDFAIGSBIl7vjqaYcWLNIGtg',
     cookieTabName: 'Cookie'
